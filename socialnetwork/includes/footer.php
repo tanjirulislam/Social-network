bf570c1c45add6c2680b9ecb140532bdb201d527
Arafatrobin <arafatrobin98@gmail.com>
2021-09-01 14:57:47 +0600

    added all file

---
+++ socialnetwork/includes/footer.php
@@ -0,0 +1,5 @@
+<div style="text-align: center; background: black; color: white; min-height: 50px;">
+<p class="cp-text" style="padding-top: 15px;">
+    © Copyright 2021 Lab Final. All rights reserved.
+</p>
+</div>
